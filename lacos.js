d74980829885d9edda2ec143b7230518ed0e066d
jhonatan <jhonatanprates@hotmail.com>
2018-09-24 10:18:39 -0300

    Aula de Laços

---
+++ lacos.js
@@ -0,0 +1,23 @@
+// let limite = Number (prompt("Até que número você quer contar ?"));
+// let numero = 1;
+
+// while( numero <= limite){
+//     console.log(numero);
+    
+//     numero = numero + 1;
+// }
+
+// console.log ("Fim");
+
+
+// let tabuada = Number(prompt("DIgite um número para ver a tabuada"));
+// let limite = Number(prompt (" Até quando vai a tabuada para você"));
+// let i = 0;
+
+// while( i <= limite){
+//     console.log(`${tabuada} X ${i} = ${tabuada*i}`);
+
+//     i = i + 1;
+//     // i++; ( incremento pode ser feito assim também )
+// }
+
